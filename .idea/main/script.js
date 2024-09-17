e951e51235c50cdce67de649727628306e23f4b1
RaphaelKumba <raphael.kumbartki@students.fhnw.ch>
2024-09-17 15:00:02 +0200

    adding a observable but it's not so easy

--- .idea/main/script.js
+++ .idea/main/script.js
@@ -1,13 +1,40 @@
-
 window.onload = function(){
-
     var counter = 0;
-
     document.getElementById("counter").onmousedown = function () {
-        counter++;
-        const div = document.querySelector("div");
-        div.innerHTML = "My new text!";
-        console.log(counter);
+        document.getElementById("count").innerHTML = counter++;
+    };
+};
+
+const CountController = () => {
+    var count = 0;
+
+    const countModel = ObservableList([]); // observable array of Todos, this state is private
+
+    const Update = () => {
+        const countAttr = Observable(count++);
+        console.log("Counter: " + count);
+        return {
+            onCountChanged: countAttr.onChange
+        }
+    }
+
+
+    const newUpdate = () => {
+        const newUpdate = Update();
+        countModel.add(newUpdate);
+        return newUpdate;
     };
 
+    return {
+        update: newUpdate()
+    }
+};
+
+const CountView = (countController, rootElement) => {
+    const count = counting => {
+        document.getElementById("count").innerHTML = counter++;
+    }
+    // binding
+
+    countController.onCountChanged(count);
 };
