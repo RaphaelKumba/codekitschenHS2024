14b4443e0621a09340a9634d76f782a3285cbc6e
RaphaelKumba <raphael.kumbartki@students.fhnw.ch>
2024-09-19 10:11:30 +0200

    doing a calculator as project

--- .idea/main/script.js
+++ .idea/main/script.js
@@ -1,17 +1,3 @@
 window.onload = function(){
-    var counter = 0;
-
-    const updateElements = count => {
-        document.getElementById("count").innerHTML = count;
-        document.getElementById("count-total").innerHTML = count;
-    }
-
-    document.getElementById("counter").onmousedown = function () {
-        updateElements(++counter);
-    };
-
-    document.getElementById("counter-reset").onmousedown = function () {
-        updateElements(counter = 0);
-    };
 
 };
