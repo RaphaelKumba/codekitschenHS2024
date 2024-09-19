3cbef778836367d0a1d65965a6215192afcac889
RaphaelKumba <raphael.kumbartki@students.fhnw.ch>
2024-09-19 12:29:48 +0200

    basic setup

--- .idea/main/script.js
+++ .idea/main/script.js
@@ -1,3 +1,24 @@
 window.onload = function(){
+    const calcElements = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "/", "x", "+", "-", ".", "="]
 
+    var displayText = "";
+    var noFloat = true;
+
+    const manageInput = input => {
+        if (input === ".") noFloat = false;
+        if (input === "=") {
+            // Calc
+            // reset stuff
+        } else if (input !== "." || (input === "." && noFloat)) {
+            displayText += input;
+            document.getElementById("display-text").innerHTML = displayText;
+        }
+
+    }
+
+    for (const key in calcElements) {
+        document.getElementById(calcElements[key]).onmouseup = function () {
+            manageInput(calcElements[key]);
+        };
+    }
 };
