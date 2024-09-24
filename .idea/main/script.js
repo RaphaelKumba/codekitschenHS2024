6a756f4b297b766dab5aa33871882f9d5d542c72
RaphaelKumba <raphael.kumbartki@students.fhnw.ch>
2024-09-24 14:17:58 +0200

    fixed errors

--- .idea/main/script.js
+++ .idea/main/script.js
@@ -1,37 +1,42 @@
-var display   = "";
-var number    = "";
-var isFloat = false;
-var computable= false;
+var display       = "";
+var number        = "";
+var isFloat     = false;
+var computable  = false;
+var tasks          = [];
 
-var tasks = [];
-
-const buttons = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "÷", "×", "+", "-", "%", ".", "=", "AC"];
-const numbers      = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const operations   = ["÷", "×", "+", "-", "%"];
+const buttons    = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "÷", "×", "+", "-", "%", ".", "=", "AC", "⌫"];
+const numbers    = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const operations = ["÷", "×", "+", "-", "%"];
 
 const validKey = key => {
     if (buttons.includes(key))      return key;
     if (key === "/")                return "÷";
     if (key === "x" || key === "*") return "×";
     if (key === "Enter")            return "=";
+    if (key === "Backspace")        return "⌫";
     return false;
 }
 
-const updateDisplay = () => document.getElementById("display-text").innerHTML = display;
-const resetDisplay = () => document.getElementById("display-text").innerHTML = display = "0";
-const setDisplay = input => document.getElementById("display-text").innerHTML = display = input;
-const displayAddNumber = nbr => { if (display === "0") display = nbr; else display += nbr; updateDisplay(); }
-const displayAddOperation = opp => { display += " " + opp + " "; updateDisplay(); }
+const updateDisplay = ()    => document.getElementById("display-text").innerHTML = display;
+const resetDisplay = ()     => document.getElementById("display-text").innerHTML = display = "0";
+const setDisplay = input           => document.getElementById("display-text").innerHTML = display = input;
+const displayAddNumber = nbr       => { if (display === "0") display = nbr; else display += nbr; updateDisplay(); }
+const displayAddFloat = ()   => { display += "."; updateDisplay(); }
+const displayAddOperation = opp    => { display += " " + opp + " "; updateDisplay(); }
+const displayDeleteLast = ()=> document.getElementById("display-text").innerHTML = display = display.substring(0, display.length - 1);
 
-const isNumber = input => numbers.includes(input);
-const setFloat = input => input === "." && number.length > 0 && !isFloat;
-const isOperation = input => operations.includes(input);
-const isCompute = input => input === "=" && tasks.length > 1;
-const reset = () => { number = ""; isFloat = false; }
+const isNumber = input           => numbers.includes(input);
+const setFloat = input           => input === "." && number.length > 0 && !isFloat;
+const reset = ()           => { number = "0"; isFloat = false; }
+const isOperation = input        => operations.includes(input);
+const isCompute = input          => input === "=" && tasks.length > 1;
+const isDelete = input           => input === "⌫" && number > 0;
+
 
 const computeTasks = () => {
     let res= 0;
     while (tasks.length > 1) {
+        console.log(tasks);
         let left = parseFloat(tasks.shift());
         let operation = tasks.shift();
         let right = parseFloat(tasks.shift());
@@ -49,7 +54,7 @@ const computeTasks = () => {
 
 const manageInput = input => {
 
-    if (isNumber(input)) {
+    if (isNumber(input) && tasks.length !== 1) {
         computable = true;
         number += input;
         displayAddNumber(input);
@@ -59,22 +64,26 @@ const manageInput = input => {
         computable = false;
         isFloat = true;
         number += input;
-        displayAddNumber(input);
+        displayAddFloat();
     }
 
     if (isOperation(input) && computable) {
         computable = false;
-        tasks.push(number);
+        if (tasks.length !== 1) tasks.push(number);
         tasks.push(input);
         reset();
         displayAddOperation(input);
     }
 
     if (isCompute(input) && computable) {
-        computable = false;
         tasks.push(number);
         setDisplay(computeTasks());
         reset();
+    }
+
+    if (isDelete(input)) {
+        number = number.substring(0, number.length - 1);
+        displayDeleteLast();
     }
 
     if (input === "AC") {
